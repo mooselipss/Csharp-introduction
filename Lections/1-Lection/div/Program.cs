26fd4b6297c9b3a1506263090a838b7cf292f380
Alex Makarov <lastcallforalcoholik@gmail.com>
2023-02-23 21:49:27 +0600

    Wrote division programm

---
+++ Lections/1-Lection/div/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Enter first number");
+double firstnumber = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter second number");
+double secondnumber = double.Parse(Console.ReadLine()!);
+Console.WriteLine(firstnumber / secondnumber);
