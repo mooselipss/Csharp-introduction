6a06cb49d8ac517aadbc1f0aadbb296752372e00
Alex Makarov <lastcallforalcoholik@gmail.com>
2023-02-23 22:07:01 +0600

    Wrote programm with new Random().Next construction

---
+++ Lections/1-Lection/new-random-next/Program.cs
@@ -0,0 +1,6 @@
+﻿int number1 = new Random().Next(1, 101);
+int number2 = new Random().Next(1, 101);
+Console.WriteLine(number1);
+Console.WriteLine(number2);
+int result = number1 + number2;
+Console.WriteLine(result);
