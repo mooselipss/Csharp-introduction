a3dffd1ff838f09ac66d1d6e85821d756cbc8feb
Alex Makarov <lastcallforalcoholik@gmail.com>
2023-02-23 21:40:07 +0600

    Wrote programm sum

---
+++ Lections/1-Lection/Sum/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Enter your first number");
+int firstnumber = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter your second number");
+int secondnumber = int.Parse(Console.ReadLine()!);
+Console.WriteLine (firstnumber + secondnumber);
