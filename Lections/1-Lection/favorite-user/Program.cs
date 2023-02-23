dd855ba7dd7f97365fb6742fdaab18588e07980c
Alex Makarov <lastcallforalcoholik@gmail.com>
2023-02-23 22:21:27 +0600

    Wrote hello favorite user programm

---
+++ Lections/1-Lection/favorite-user/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine()!;
+if (name.ToLower() == "alex")
+{
+    Console.WriteLine("Cool bro, you are comeback!!!");
+}
+else
+{
+    Console.WriteLine($"Hello, {name}");
+}
