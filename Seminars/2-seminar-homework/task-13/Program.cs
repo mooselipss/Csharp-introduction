7ca645e3add8c557d9ceca58bd9b1e6de8b85446
Alex Makarov <lastcallforalcoholik@gmail.com>
2023-03-05 16:38:55 +0600

    add homework for seminar 2

---
+++ Seminars/2-seminar-homework/task-13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+void GetThirdDigit(int number)
+{
+    if (number < 100) Console.WriteLine("Third digit not found");
+    else
+    {
+        while (number >= 999)
+        {
+            number = number / 10;
+
+        }
+        int result = number % 10;
+
+        Console.WriteLine(result);
+
+    }
+}
+
+Console.WriteLine("Enter your number: ");
+int num = int.Parse(Console.ReadLine()!);
+GetThirdDigit(num);
---
+++ Seminars/2-seminar-homework/task-15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+void DayOfRest(int day)
+{
+    if (day > 7) Console.WriteLine("Incorrect input, try again");
+    else if (day <= 5) Console.WriteLine("No");
+    else
+    {
+        Console.WriteLine("Yes");
+    }
+}
+
+
+Console.WriteLine("Enter a number from 1 to 7: ");
+int numberOfDay = int.Parse(Console.ReadLine()!);
+DayOfRest(numberOfDay);
