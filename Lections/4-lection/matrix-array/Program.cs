930690c6d18b9f9a4dc6e5ff80c0881f3dfd3674
Alex Makarov <lastcallforalcoholik@gmail.com>
2023-03-13 12:36:19 +0600

    add lection 4 not completely

---
+++ Lections/4-lection/matrix-array/Program.cs
@@ -0,0 +1,25 @@
+﻿void PrintArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            Console.Write($"{matr[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            matr[rows, columns] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+PrintArray(matrix);
