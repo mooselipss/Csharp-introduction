5086d710e689df28c5a890efceec4938e9d19f3e
Alex Makarov <lastcallforalcoholik@gmail.com>
2023-03-20 19:05:37 +0600

    add lection 7 not full

---
+++ Lections/7-lection/task-1_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Соберите строку с числами от А до Б, А >= Б.
+
+// string NumbersFor(int a, int b)
+// {
+//     string result = string.Empty;
+//     for (int i = a; i >= b; i--)
+//     {
+//         result += $"{i} ";
+//     }
+//     return result;
+// }
+
+string NumbersRecursion(int a, int b)
+{
+    if (a >= b) return  $"{a} " + NumbersRecursion(a - 1, b);
+    else return string.Empty;
+}
+
+Console.WriteLine("Enter number a: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter number b: ");
+int numberB = int.Parse(Console.ReadLine()!);
+
+string res = NumbersRecursion(numberA, numberB);
+Console.WriteLine(res);
