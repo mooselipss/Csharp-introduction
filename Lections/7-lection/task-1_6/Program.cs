a9eeae1ef8ca05251e3cd56096330c1fbbec9f60
Alex Makarov <lastcallforalcoholik@gmail.com>
2023-03-25 17:22:18 +0600

    add lection 7 full

--- Lections/7-lection/task-1_6/Program.cs
+++ Lections/7-lection/task-1_6/Program.cs
@@ -1,9 +1,28 @@
 ﻿// Получаем информацию о папках и файлах
-string path = "/home/alexmakar/dev/Csharp-introduction/Lections/1-Lection/div";
-DirectoryInfo di = new DirectoryInfo(path);
-Console.WriteLine(di.CreationTime);
-FileInfo[] fi = di.GetFiles();
-for (int i = 0; i < fi.Length; i++)
+// string path = "/home/alexmakar/dev/Csharp-introduction/Lections/1-Lection/div";
+// DirectoryInfo di = new DirectoryInfo(path);
+// Console.WriteLine(di.CreationTime);
+// FileInfo[] fi = di.GetFiles();
+// for (int i = 0; i < fi.Length; i++)
+// {
+//     Console.WriteLine(fi[i].Name);
+// }
+
+void CatalogInfo(string path, string indent = "")
 {
-    Console.WriteLine(fi[i].Name);
+    DirectoryInfo catalog = new DirectoryInfo(path);
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+    FileInfo[] files = catalog.GetFiles();
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
 }
+
+string path = "/Users/alexandrmakarov/dev/Csharp-introduction/Lections/1-Lection";
+CatalogInfo(path);
---
+++ Lections/7-lection/task-1_8/Program.cs
@@ -0,0 +1,47 @@
+﻿// числа фибоначи методом рекурсии и итеративным методом
+// подсчет кол-ва циклов и врмени на выполнение
+
+decimal fibrec = 0;
+decimal fibiter = 0;
+
+decimal FibonacciRecursion(int n)
+{
+    fibrec++;
+    return n == 0 || n == 1 ? 1 : FibonacciRecursion(n - 1) + FibonacciRecursion(n - 2);
+}
+
+decimal FibonacciIteration(int n)
+{
+    fibiter++;
+    decimal result = 1;
+    decimal f0 = 1;
+    decimal f1 = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        result = f0 + f1;
+        f0 = f1;
+        f1 = result;
+    }
+    return result;
+}
+
+Console.ReadLine();
+DateTime dt = DateTime.Now;
+
+for (int n = 10; n <= 40; n++)
+{
+    Console.WriteLine($"Fibonacci iteration({n}) = {FibonacciIteration(n)} кол-во циклов = {fibiter}");
+}
+Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+
+Console.WriteLine();
+
+Console.ReadLine();
+dt = DateTime.Now;
+
+for (int n = 10; n <= 40; n++)
+{
+    Console.WriteLine($"Fibonacci recursion({n}) = {FibonacciRecursion(n)} кол-во циклов = {fibrec}");
+}
+Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+
