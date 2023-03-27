1342893df0326976280d25c6bae7c01c63b463c7
Alex Makarov <lastcallforalcoholik@gmail.com>
2023-03-27 17:23:14 +0600

    add homework for 9 seminar

---
+++ Seminars/9-seminar-homework/task-1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все
+// чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+void GetEvenNumber(int start, int end)
+{
+    if (end == 0) return;
+    else if (end % 2 != 0)
+    end -= 1;
+    GetEvenNumber(start, end - 2);
+    Console.Write($"{end} \t");
+
+}
+int start = 1;
+int end = 7;
+GetEvenNumber(start, end);
---
+++ Seminars/9-seminar-homework/task-2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int SumNum(int start, int end)
+{
+    if(end == 0) return 0;
+    return end + SumNum(start, end - 1);
+}
+
+int start = 1;
+int end = 15;
+int resSum = SumNum(start, end);
+Console.WriteLine(resSum);
